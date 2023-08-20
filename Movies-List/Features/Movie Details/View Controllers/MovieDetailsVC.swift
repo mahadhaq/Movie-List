c3d56aa89cbf936d453645504c87f83f26910927
Mahad <m.haq@70@gmail.com>
2023-08-21 03:15:41 +0500

    Set date fetched from api call.

--- Movies-List/Features/Movie Details/View Controllers/MovieDetailsVC.swift
+++ Movies-List/Features/Movie Details/View Controllers/MovieDetailsVC.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class MovieDetailsVC: UIViewController {
     // MARK: - Properties
-    
+    var viewModel = MovieDetailsViewModel()
     
     // MARK: - IBOutlets
     
@@ -27,8 +27,13 @@ class MovieDetailsVC: UIViewController {
     // MARK: - Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        navigationItem.title = "Movie Details"
+        viewModel.set(view: self)
         // Do any additional setup after loading the view.
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.viewModel.fetchMovieDetails()
     }
 
 
@@ -42,6 +47,39 @@ class MovieDetailsVC: UIViewController {
     }
     */
 
+    func calculateTime(_ timeValue: Int) -> String {
+        let timeMeasure = Measurement(value: Double(timeValue), unit: UnitDuration.minutes)
+        let hours = timeMeasure.converted(to: .hours)
+        if hours.value > 1 {
+            let minutes = timeMeasure.value.truncatingRemainder(dividingBy: 60)
+            return String(format: "%.f %@ %.f %@", hours.value, "h", minutes, "m")
+        }
+        return String(format: "%.f %@", timeMeasure.value, "m")
+    }
 }
 
 // MARK: - Extensions
+
+extension MovieDetailsVC: MovieDetailsViewDelegate {
+    func successfullyReceivedMovieDetails() {
+        if let movie = viewModel.movieDetail {
+            self.backdropImgView.sd_setImage(with: movie.backdropURL, completed: nil)
+            self.posterImgView.sd_setImage(with: movie.posterURL, completed: nil)
+            self.titleLbl.text = movie.title
+            self.overviewLbl.text = movie.overview
+            self.tagline.text = movie.tagline
+            self.dateTimeLbl.text = "\(movie.releaseDate ?? "") \u{2022} \(self.calculateTime(movie.runtime)) "
+            self.GenreLbl.text = movie.genres.map { $0.name }.joined(separator: ", ")
+        }
+    }
+    
+    func failedToReceivedMovieDetails() {
+        self.backdropImgView.sd_setImage(with: viewModel.selectedMovie.backdropURL, completed: nil)
+        self.posterImgView.sd_setImage(with: viewModel.selectedMovie.posterURL, completed: nil)
+        self.titleLbl.text = viewModel.selectedMovie.title
+        self.overviewLbl.text = viewModel.selectedMovie.overview
+        self.dateTimeLbl.text = viewModel.selectedMovie.releaseDate
+    }
+    
+    
+}
