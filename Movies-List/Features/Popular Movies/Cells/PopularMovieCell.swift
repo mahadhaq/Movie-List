9baec53f0d57d0d2b830aab4ff06b763ca9fa431
Mahad <m.haq@70@gmail.com>
2023-08-20 16:12:26 +0500

    Created/Design of popularMovieCell

---
+++ Movies-List/Features/Popular Movies/Cells/PopularMovieCell.swift
@@ -0,0 +1,21 @@
+//
+//  PopularMovieCell.swift
+//  Movies-List
+//
+//  Created by Mahad on 8/20/23.
+//
+
+import UIKit
+
+class PopularMovieCell: UICollectionViewCell {
+
+    @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet weak var titleLbl: UILabel!
+    @IBOutlet weak var descriptionLbl: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
