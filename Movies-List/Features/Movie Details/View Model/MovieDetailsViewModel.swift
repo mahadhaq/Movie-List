ad5335124131bf4a66761bdf83b1052a0cbc61b7
Mahad <m.haq@70@gmail.com>
2023-08-21 03:15:17 +0500

    Api Implementation to fetch data.

--- Movies-List/Features/Movie Details/View Model/MovieDetailsViewModel.swift
+++ Movies-List/Features/Movie Details/View Model/MovieDetailsViewModel.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+
+protocol MovieDetailsViewDelegate: AnyObject {
+    func successfullyReceivedMovieDetails()
+    func failedToReceivedMovieDetails()
+}
+
+class MovieDetailsViewModel {
+    // MARK: - Properties
+    var movieDetail:MovieDetails?
+    weak var delegate: MovieDetailsViewDelegate?
+    var selectedMovie: MovieSummary!
+    
+    // MARK: - Set up
+    
+    func set(view: MovieDetailsViewDelegate) {
+        self.delegate = view
+    }
+    
+    func fetchMovieDetails() {
+        NetworkClient.shared.getRequest(EndPoints.BASE_URL + "/\(selectedMovie.id)", parameters: nil, headers: ApiHeader.header) { (movie: MovieDetails) in
+            self.movieDetail = movie
+            self.delegate?.successfullyReceivedMovieDetails()
+        } failure: { error in
+            self.delegate?.failedToReceivedMovieDetails()
+        }
+
+    }
+}
