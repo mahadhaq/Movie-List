ef9513dbb3926dffa1eef1b541677b5f3ff5fe49
Mahad <m.haq@70@gmail.com>
2023-08-21 02:26:47 +0500

    Added Model File

---
+++ Movies-List/Features/Movie Details/Model/MovieDetail.swift
@@ -0,0 +1,37 @@
+//
+//  MovieDetail.swift
+//  Movies-List
+//
+//  Created by Mahad on 8/21/23.
+//
+
+import Foundation
+
+public struct MovieDetails: Codable {
+    public let genres: [Genre]
+    public let backdropPath: String
+    public let id: Int
+    public let overview: String
+    public let posterPath: String
+    public let releaseDate: String
+    public let tagline: String
+    public let title: String
+    public let runtime: Int
+    
+    
+    enum CodingKeys: String, CodingKey {
+        case genres
+        case id
+        case overview
+        case posterPath = "poster_path"
+        case releaseDate = "release_date"
+        case tagline
+        case title
+        case backdropPath = "backdrop_path"
+        case runtime
+    }
+    
+    public struct Genre: Codable {
+        public let name: String
+    }
+}
